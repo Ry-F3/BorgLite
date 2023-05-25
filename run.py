dfda72897c7b3ce58e3383223afe32245a047bdd
Ry-F3 <rhysfeew@gmail.com>
2023-05-25 17:11:44 +0000

    Change - opening page functionality and spearate into game initialization. Simplify player choices to get the programe running. Created system objects to be in a loop

--- run.py
+++ run.py
@@ -1,30 +1,35 @@
-import random 
+import random
 import sys
 
 # Define the Player class
-
 class Player:
     def __init__(self):
         self.health = 100
         self.defence = 10
         self.attack = 10
         self.level = 1
-        
+
     def increase_attack(self):
         self.attack += 5
-        
+
     def increase_defence(self):
         self.defence += 5
-    
+
     def take_damage(self, damage):
-        damage -= self.defence
-        if damage > 0:
+        if damage > self.defence:
+            damage -= self.defence
             self.health -= damage
-            
-    def is_alive():
+        else:
+            self.defence -= damage
+            if self.defence < 0:
+                self.defence = 0
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self):
         return self.health > 0
-    
-    def display_stats():
+
+    def display_stats(self):
         print("Player Stats:")
         print(f"Player Level: {self.level}")
         print(f"Player Health: {self.health}")
@@ -32,187 +37,211 @@ class Player:
         print(f"Player Defence: {self.defence}")
 
 
-# Define the Planet Class 
-
-class Planet: 
+# Define the Planet Class
+class Planet:
     def __init__(self, name, resources, level):
         self.name = name
         self.resources = resources
         self.assimilated = False
-        self.level = random.randint(1, 20)
-        self.attack_points = random.randomint(1, 5) + level
+        self.level = level
+        self.attack_points = random.randint(1, 5) + level
         self.defence_points = random.randint(1, 5) + level
-    
+
     def attack_player(self, player):
         damage_dealt = self.attack_points
         player.take_damage(damage_dealt)
         print(self.name, "attacks! Player takes", damage_dealt, "damage.")
-        
-    def assimilated(self):
+        print("Player health remaining:", player.health)
+        if not player.is_alive():
+            print("Player has been assimilated. Game Over.")
+            sys.exit()
+
+    def assimilate(self):
         self.assimilated = True
-        
+
     def is_assimilated(self):
         return self.assimilated
-    
-# Define System Class 
 
+
+# Define System Class
 class System:
-    def __init__ (self, enemy_resistance, planets):
+    def __init__(self, name, enemy_resistance, planets):
         self.name = name
         self.enemy_resistance = enemy_resistance
         self.planets = planets
-        
+
     def assimilate_planet(self, planet_index):
         if planet_index in range(len(self.planets)):
             planet = self.planets[planet_index]
             if not planet.is_assimilated():
                 planet.assimilate()
-                self.resources_gained = planet.resources
-                planet.resources = {"processing": 0}
                 return True
             else:
                 print(planet.name, "is already assimilated!")
         else:
             print("Invalid selection")
         return False
-    
+
     def attack(self, attack_power):
         success_chance = attack_power / self.enemy_resistance
         return random.random() <= success_chance
-    
+
     def display_planets(self):
         for i, planet in enumerate(self.planets):
             print(f"{i+1}. {planet.name}")
-    
-    # def add_planet(self, planet):
-    #     self.planets.append(planet)
-        
+
+
 # Load systems data
 def load_systems_data():
     systems_data = [
         {
             "name": "Alpha System",
-            "enemy_resistance": random.randint(40, 70),
+            "enemy_resistance": random.randint(1, 20),
             "planets": [
-                Planet("Earth", {"processing": random.randint(20, 50)}),
-                Planet("Vulcan", {"processing": random.randint(20, 50)}),
-                Planet("Klingon", {"processing": random.randint(20, 50)}),
-                Planet("Romulus", {"processing": random.randint(20, 50)}),
-                Planet("Andoria", {"processing": random.randint(20, 50)})
+                Planet("Earth", {"processing": random.randint(20, 50)}, 1),
+                Planet("Vulcan", {"processing": random.randint(20, 50)}, 1),
+                Planet("Klingon", {"processing": random.randint(20, 50)}, 1),
+                Planet("Romulus", {"processing": random.randint(20, 50)}, 1),
+                Planet("Andoria", {"processing": random.randint(20, 50)}, 1)
             ]
         },
         {
             "name": "Beta System",
-            "enemy_resistance": random.randint(40, 70),
+            "enemy_resistance": random.randint(1, 20),
             "planets": [
-                Planet("Cardassia Prime", {"processing": random.randint(20, 50)}),
-                Planet("Bajor", {"processing": random.randint(20, 50)}),
-                Planet("Ferenginar", {"processing": random.randint(20, 50)}),
-                Planet("Qo'noS", {"processing": random.randint(20, 50)}),
-                Planet("Trill", {"processing": random.randint(20, 50)})
+                Planet("Cardassia Prime", {"processing": random.randint(20, 50)}, 1),
+                Planet("Bajor", {"processing": random.randint(20, 50)}, 1),
+                Planet("Ferenginar", {"processing": random.randint(20, 50)}, 1),
+                Planet("Qo'noS", {"processing": random.randint(20, 50)}, 1),
+                Planet("Trill", {"processing": random.randint(20, 50)}, 1)
             ]
         },
         {
             "name": "Gamma System",
-            "enemy_resistance": random.randint(40, 70),
+            "enemy_resistance": random.randint(1, 20),
             "planets": [
-                Planet("Betazed", {"processing": random.randint(20, 50)}),
-                Planet("Risa", {"processing": random.randint(20, 50)}),
-                Planet("Tellar Prime", {"processing": random.randint(20, 50)}),
-                Planet("Q'uorath", {"processing": random.randint(20, 50)}),
-                Planet("Federation Outpost 112", {"processing": random.randint(20, 50)})
+                Planet("Betazed", {"processing": random.randint(20, 50)}, 1),
+                Planet("Risa", {"processing": random.randint(20, 50)}, 1),
+                Planet("Tellar Prime", {"processing": random.randint(20, 50)}, 1),
+                Planet("Q'uorath", {"processing": random.randint(20, 50)}, 1),
+                Planet("Federation Outpost 112", {"processing": random.randint(20, 50)}, 1)
             ]
         },
         {
             "name": "Delta System",
-            "enemy_resistance": random.randint(40, 70),
+            "enemy_resistance": random.randint(1, 100),
             "planets": [
-                Planet("Vorta", {"processing": random.randint(20, 50)}),
-                Planet("Founders' Homeworld", {"processing": random.randint(20, 50)}),
-                Planet("Breen Homeworld", {"processing": random.randint(20, 50)}),
-                Planet("Talax", {"processing": random.randint(20, 50)}),
-                Planet("Ocampa", {"processing": random.randint(20, 50)})
+                Planet("Vorta", {"processing": random.randint(20, 50)}, 1),
+                Planet("Founders' Homeworld", {"processing": random.randint(20, 50)}, 1),
+                Planet("Breen Homeworld", {"processing": random.randint(20, 50)}, 1),
+                Planet("Talax", {"processing": random.randint(20, 50)}, 1),
+                Planet("Ocampa", {"processing": random.randint(20, 50)}, 1)
             ]
         },
         {
             "name": "Epsilon System",
-            "enemy_resistance": random.randint(40, 70),
+            "enemy_resistance": random.randint(1, 20),
             "planets": [
-                Planet("Nimbus III", {"processing": random.randint(20, 50)}),
-                Planet("Rigel VII", {"processing": random.randint(20, 50)}),
-                Planet("Sheliak", {"processing": random.randint(20, 50)}),
-                Planet("Tribble Homeworld", {"processing": random.randint(20, 50)}),
-                Planet("Deneb IV", {"processing": random.randint(20, 50)})
+                Planet("Nimbus III", {"processing": random.randint(20, 50)}, 1),
+                Planet("Rigel VII", {"processing": random.randint(20, 50)}, 1),
+                Planet("Sheliak", {"processing": random.randint(20, 50)}, 1),
+                Planet("Tribble Homeworld", {"processing": random.randint(20, 50)}, 1),
+                Planet("Deneb IV", {"processing": random.randint(20, 50)}, 1)
             ]
         }
     ]
-    return systems_data  
+    return systems_data
 
-# Assimilate a planet within a system 
 
+# Assimilate a planet within a system
 def assimilate_planet(system, player):
     print("\nAvailable Systems for attack: ")
     for i, system in enumerate(systems):
-        print(f"{i+1}. {system.name}")
-        
+        print(f"{i + 1}. {system.name}")
+
     system_index = int(input("Select a system to attack: ")) - 1
     selected_system = systems[system_index]
     print(f"\nAvailable planets for attack in {selected_system.name}:")
     selected_system.display_planets()
-    
+
     planet_index = int(input("Select a planet to attack: ")) - 1
     assimilated = selected_system.assimilate_planet(planet_index)
     if assimilated:
         player.level += 1
         player.increase_attack()
         player.increase_defence()
-        print("You have assimilated", assimilated.name)
+        print("\n=================================================================")
+        print("                    You have assimilated", selected_system.planets[planet_index].name)
+        print("=================================================================\n")
     else:
         print("Assimilation failed!")
-        
-# Attack a system
 
+
+# Attack a system
 def attack_system(system, player):
     print("\nAttacking", system.name, "with resistance level:", system.enemy_resistance)
-    if success: 
-        print("We are Borg. Existance as you know it is over")
+    success = system.attack(player.attack)
+    if success:
+        print("We are Borg. Existence as you know it is over. We will add your biological and technological distinctiveness to our own. Resistance is futile.\n")
         assimilate_planet(system, player)
     else:
-        print("Resistance was too strong. Assimilation failed.")
-        player.take_damage(system.enemy_resistance)
+        print("Our attack was unsuccessful. Prepare for counterattack!\n")
+        system.planets[random.randint(0, len(system.planets) - 1)].attack_player(player)
+        decrease_player_life(player, system.planets[random.randint(0, len(system.planets) - 1)].attack_points)
 
-# Decrease player life 
 
-def decrease_player_life(player):
-    player.health -= damage_dealt
+# Decrease player life
+def decrease_player_life(player, damage_dealt):
+    player.take_damage(damage_dealt)
     print("Your collective has been damaged! Player health remaining:", player.health)
-    
-        
-# Main game loop
+    if not player.is_alive():
+        print("Your collective has been destroyed. Game over.")
+        sys.exit()
 
-def main_game_loop(player, systems):
-    
-    while True:
-        print("\nPlayers: Turn")
-        player.display_stats()
-        break
-        
-    
 
-# Display opening screen 
+# Game over
+def game_over():
+    print("\nGAME OVER")
+    sys.exit()
 
-def opening_page():
-    
-    print("=== BorgLite ===\n")
-    print("Welcome to BorgLite!")
-    choice = input("Do you wish to become part of the collective? (Y/N) ")
-    if choice.upper() == "Y":
-        print("Welcome! Prepare to assimilate the digital realm.")
-        main_game_loop()
+
+# Game initialization
+player = Player()
+systems_data = load_systems_data()
+systems = []
+
+# Create system objects
+for system_data in systems_data:
+    system = System(system_data["name"], system_data["enemy_resistance"], system_data["planets"])
+    systems.append(system)
+
+# Game loop
+print("============================================")
+print("            Welcome to BorgLite             ")
+print("============================================")
+
+while True:
+    player.display_stats()
+    print("\nWhat would you like to do?")
+    print("1. Attack a system")
+    print("2. Quit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("\nAvailable Systems for attack: ")
+        for i, system in enumerate(systems):
+            print(f"{i + 1}. {system.name}")
+
+        system_index = int(input("Select a system to attack: ")) - 1
+        selected_system = systems[system_index]
+        attack_system(selected_system, player)
+
+        if not player.is_alive():
+            game_over()
+
+    elif choice == "2":
+        game_over()
+
     else:
-        print("Your resistance is futile. Goodbye!")
-
-# Run the game
-if __name__ == "__main__":
-    
-    opening_page()
+        print("Invalid choice. Please try again.")
