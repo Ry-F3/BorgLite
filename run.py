be2dcec82a1785b428f896aef18141903dc27ef1
Ry-F3 <rhysfeew@gmail.com>
2023-05-25 12:57:20 +0000

    Add initial opening page functions

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+import sys
+
+# Main game loop
+def main_game_loop():
+    
+    while True:
+        
+    
+
+# Display opening screen 
+def opening_page():
+    
+    print("=== BorgLite ===\n")
+    print("Welcome to BorgLite!")
+    choice = input("Do you wish to become part of the collective? (Y/N) ")
+    if choice.upper() == "Y":
+        print("Welcome! Prepare to assimilate the digital realm.")
+        main_game_loop()
+    else:
+        print("Your resistance is futile. Goodbye!")
+
+# Run the game
+if __name__ == "__main__":
+    
+    opening_page()
